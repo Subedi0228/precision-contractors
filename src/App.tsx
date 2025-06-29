f28835d5be94843d898d10fc8b08754d193c54d5
Subedi0228 <chiransubedi2001@gmail.com>
2025-06-28 22:27:52 -0500

    Feat: Contact us section divider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
 import Services from './components/services/Services';
-// import ContactSection from './components/contact/ContactSection';
+import Contact from './components/contact/Contact';
+
 // import Footer from './components/footer/Footer';
 import './App.css';
 
@@ -11,7 +12,7 @@ function App() {
       <Header />
       <Hero />
       <Services />
-      {/* <ContactSection /> */}
+      <Contact />
       {/* <Footer /> */}
       
       {/* Temporary content for development */}
---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,16 @@
+import './Services.css';
+
+const Contact = () => {
+  return (
+    <header className="contact">
+      <div className="contact_header">
+        <div className="contact_header_divider"></div>
+        <div className="contact_header_text">
+            <p>OUR SERVICES</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Contact;
